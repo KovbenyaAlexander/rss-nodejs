152b77ed3dc69a7dfbd9fa8d4d859a3317120f48
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2022-12-07 15:43:01 +0300

    implement correct working with absolute path in "add" task

--- src/add.js
+++ src/add.js
@@ -1,12 +1,22 @@
 import { isFolderExist } from "./utils.js";
 import { appendFile } from "node:fs/promises";
+import path from "path";
 
 const add = async (directory, fileName) => {
-  if (await isFolderExist(`${directory}/${fileName}`)) {
-    console.log("File already exists");
+  if (path.isAbsolute(fileName)) {
+    if (await isFolderExist(`${fileName}`)) {
+      console.log("File already exists");
+    } else {
+      appendFile(`${fileName}`, "");
+      console.log("File created successfully");
+    }
   } else {
-    appendFile(`${directory}/${fileName}`, "");
-    console.log("File created successfully");
+    if (await isFolderExist(`${directory}/${fileName}`)) {
+      console.log("File already exists");
+    } else {
+      appendFile(`${directory}/${fileName}`, "");
+      console.log("File created successfully");
+    }
   }
 };
 
