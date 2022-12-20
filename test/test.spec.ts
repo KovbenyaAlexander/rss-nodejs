7fc78b32344224bab9b12dccd21bbd249ff2b49b
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2022-12-20 15:17:17 +0300

    add test scenario 3

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -3,14 +3,15 @@ import { app } from "../src/index";
 
 const testApi = supertest(app);
 const LINK = "/api/users";
+const VALID_ID_EXAMPLE = "11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000";
+
+const newUser = {
+  username: "1233113",
+  age: 25,
+  hobbies: ["js"],
+};
 
 describe("Test scenario 1", () => {
-  const newUser = {
-    username: "1233113",
-    age: 25,
-    hobbies: ["js"],
-  };
-
   let userId: string;
 
   it("Should get an empty db", async () => {
@@ -20,7 +21,7 @@ describe("Test scenario 1", () => {
   });
 
   it("should add user", async () => {
-    const response = await testApi.post(LINK).set("Content-Type", "application/json").send(JSON.stringify(newUser));
+    const response = await testApi.post(LINK).send(JSON.stringify(newUser));
     expect(response.statusCode).toBe(201);
     const body = JSON.parse(response.body);
 
@@ -82,13 +83,69 @@ describe("Test scenario 2", () => {
     expect(response.statusCode).toBe(400);
   });
 
+  it("Should get status 400 if userId is not valid - PUT", async () => {
+    const response = await testApi.put(`${LINK}/wrong_id`).send(JSON.stringify({ age: 111 }));
+    expect(response.statusCode).toBe(400);
+  });
+
+  it("Should get status 404 if user not found - PUT", async () => {
+    const response = await testApi.put(`${LINK}/${VALID_ID_EXAMPLE}`).send(JSON.stringify({ age: 111 }));
+    expect(response.statusCode).toBe(404);
+  });
+
   it("Should get status 400 if userId is not valid - DELETE", async () => {
-    const response = await testApi.delete(`${LINK}/wrong_userid`);
+    const response = await testApi.delete(`${LINK}/wrong_id`);
     expect(response.statusCode).toBe(400);
   });
 
   it("Should get status 404 if user not found - DELETE", async () => {
-    const response = await testApi.delete(`${LINK}/11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000`);
+    const response = await testApi.delete(`${LINK}/${VALID_ID_EXAMPLE}`);
     expect(response.statusCode).toBe(404);
+  });
+});
+
+describe("Test scenario 3", () => {
+  const userIDs: string[] = [];
+
+  it("Should get empty users arr", async () => {
+    const response = await testApi.get(`${LINK}`);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toBe("[]");
+  });
+
+  it("Should add user", async () => {
+    const response = await testApi.post(LINK).send(JSON.stringify(newUser));
+    expect(response.statusCode).toBe(201);
+    userIDs.push(JSON.parse(response.body).id);
+  });
+
+  it("Should add user", async () => {
+    const response = await testApi.post(LINK).send(JSON.stringify(newUser));
+    expect(response.statusCode).toBe(201);
+    userIDs.push(JSON.parse(response.body).id);
+  });
+
+  it("Should get 2 users", async () => {
+    const response = await testApi.get(`${LINK}`);
+    expect(response.statusCode).toBe(200);
+    expect(JSON.parse(response.body).length).toBe(2);
+  });
+
+  it("Should remove user", async () => {
+    const userID = userIDs.pop();
+    const response = await testApi.delete(`${LINK}/${userID}`);
+    expect(response.statusCode).toBe(204);
+  });
+
+  it("Should remove user", async () => {
+    const userID = userIDs.pop();
+    const response = await testApi.delete(`${LINK}/${userID}`);
+    expect(response.statusCode).toBe(204);
+  });
+
+  it("Should get empty users arr", async () => {
+    const response = await testApi.get(`${LINK}`);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toBe("[]");
   });
 });
